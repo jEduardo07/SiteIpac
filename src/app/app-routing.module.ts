63b67710daf51d82f2661149f5d6366113f0a25d
joseduardo <joseduard0zeta@gmail.com>
2023-08-03 13:36:54 -0300

    site completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ 
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'product', component: ProductComponent },
+    { path: 'signUp', component: SignUpComponent },
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
