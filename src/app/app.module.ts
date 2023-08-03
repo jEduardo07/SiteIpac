ed29c4334c42132661a3223ccc12d97f59d5f66e
joseduardo <joseduard0zeta@gmail.com>
2023-08-03 13:36:29 -0300

    Site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ProductComponent } from './product/product.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    ProductComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
